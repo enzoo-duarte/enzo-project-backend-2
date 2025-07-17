07c43bdd17ce515bfc310cb0272b18cae0163774
Enzo Duarte <enzoduarte2019@gmail.com>
2025-07-17 01:40:39 -0300

    Implementacion de jwt y passport

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ const connectDB = require('./src/config/db');
 const userRoutes = require('./src/routes/user.routes');
 const sessionsRoutes = require('./src/routes/sessions.routes');
 
+const passport = require('./src/config/passport');
+
 const app = express();
 
 // Conectarse con la base de datos
@@ -15,6 +17,7 @@ app.use(express.json());
 // Rutas
 app.use('/api/users', userRoutes);
 app.use('/api/sessions', sessionsRoutes);
+app.use(passport.initialize());
 
 // Ruta de test
 app.get('/', (req, res) => {
--- src/routes/sessions.routes.js
+++ src/routes/sessions.routes.js
@@ -1,7 +1,19 @@
 const express = require('express');
 const router = express.Router();
+const passport = require('passport');
 const sessionsController = require('../controllers/sessions.controller');
 
 router.post('/login', sessionsController.login);
+
+router.get(
+    '/current',
+    passport.authenticate('jwt', { session: false }),
+    (req, res) => {
+        res.json({
+            message: 'Login successful!',
+            user: req.user
+        });
+    }
+);
 
 module.exports = router;
